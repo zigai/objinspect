da8abcd2a8dde632fea4a623537050aa284d04b3
zigai <ziga.ivansek@gmail.com>
2022-09-22 18:50:41 +0200

    feat: add process_obj function, reorganize imports

--- interfacy_core/__init__.py
+++ interfacy_core/__init__.py
@@ -1,1 +1,21 @@
-from interfacy_core import constants, interfacy_class, interfacy_func, interfacy_param, util
+import inspect
+
+from interfacy_core import util
+from interfacy_core.constants import EMPTY, SPECIAL_GENERIC_ALIAS, UNION_GENERIC_ALIAS
+from interfacy_core.exceptions import InterfacyException, UnsupportedParamError
+from interfacy_core.interfacy_class import InterfacyClass
+from interfacy_core.interfacy_func import InterfacyFunction, InterfacyParameter
+
+
+def process_obj(obj, *args):
+    if inspect.isfunction(obj):
+        return InterfacyFunction(obj)
+    if inspect.isclass(obj):
+        cls = InterfacyClass(obj)
+        for i in cls.methods.keys():
+            if i == "__init__":
+                continue
+            if i not in args:
+                del cls.methods[i]
+        return cls
+    raise TypeError(obj)
