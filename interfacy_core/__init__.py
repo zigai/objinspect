6d16236d5ca89a8a62e46e53465012a2d339025e
zigai <ziga.ivansek@gmail.com>
2022-10-09 15:17:16 +0200

    feat: more util functions

--- interfacy_core/__init__.py
+++ interfacy_core/__init__.py
@@ -1,10 +1,16 @@
 import inspect
 
-from interfacy_core import util
-from interfacy_core.constants import EMPTY, SPECIAL_GENERIC_ALIAS, UNION_GENERIC_ALIAS
+from interfacy_core.constants import (
+    ALIAS_TYPE,
+    EMPTY,
+    SPECIAL_GENERIC_ALIAS,
+    UNION_GENERIC_ALIAS,
+    UNION_TYPE,
+)
 from interfacy_core.exceptions import InterfacyException, UnsupportedParamError
 from interfacy_core.interfacy_class import InterfacyClass
 from interfacy_core.interfacy_func import InterfacyFunction, InterfacyParameter
+from interfacy_core.util import *
 
 
 def process_obj(obj, *args):
@@ -12,10 +18,18 @@ def process_obj(obj, *args):
         return InterfacyFunction(obj)
     if inspect.isclass(obj):
         cls = InterfacyClass(obj)
+        cpy = cls.methods.copy()
         for i in cls.methods.keys():
             if i == "__init__":
                 continue
-            if i not in args:
-                del cls.methods[i]
+            if i.startswith("__"):
+                del cpy[i]
+        if args:
+            for i in cls.methods.keys():
+                if i == "__init__":
+                    continue
+                if i not in args:
+                    del cpy[i]
+        cls.methods = cpy
         return cls
     raise TypeError(obj)
--- interfacy_core/util.py
+++ interfacy_core/util.py
@@ -1,6 +1,10 @@
 import os
+import types
+import typing
 
 from docstring_parser import Docstring
+
+from interfacy_core.constants import UNION_TYPE
 
 
 def has_docstring(docstring: str | None) -> bool:
@@ -19,9 +23,18 @@ def docstring_description(docstring: Docstring | None) -> str:
     return ""
 
 
-class UnionTypeParameter:
+class UnionParameter:
     def __init__(self, params: tuple) -> None:
         self.params = params
+
+    def __iter__(self):
+        for i in self.params:
+            yield i
+
+    def __repr__(self) -> str:
+        params = [type_as_str(i) for i in self.params]
+        params = " | ".join(params)
+        return f"{self.__class__.__name__}({params})"
 
 
 def type_as_str(t):
@@ -31,9 +44,36 @@ def type_as_str(t):
     return type_str.split(".")[-1]
 
 
-def extract_enum_options(e):
+def get_enum_opts(e):
     return tuple(e.__members__.keys())
 
 
 def is_file(path):
     return os.path.isfile(path)
+
+
+def simplify_type(t, depth: int = -1):
+    if depth == 0:
+        return t
+    if depth > 0:
+        depth -= 1
+    origin = typing.get_origin(t)
+    if type(origin) is types.NoneType:
+        return t
+
+    if origin in UNION_TYPE:
+        origin = typing.get_args(t)
+        return UnionParameter(tuple(simplify_type(i, depth=depth) for i in origin))
+    return simplify_type(origin, depth=depth)
+
+
+def call_bound_method(obj: object, name: str, obj_args: dict = {}, method_args: dict = {}):
+    return obj(**obj_args).__getattribute__(name)(**method_args)
+
+
+def split_type_hint(t):
+    """
+    (Base, Sub)
+    list[int] -> (list,int)
+    """
+    return typing.get_origin(t), typing.get_args(t)
