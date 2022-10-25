9e1baa1310faead403e788b5271fde3a478e71de
Žiga Ivanšek <ziga.ivansek@gmail.com>
2022-10-25 11:56:36 -0700

    Delete interfacy_core directory

--- interfacy_core/constants.py
+++
@@ -1,9 +0,0 @@
-import inspect
-import types
-import typing
-
-EMPTY = inspect._empty
-SPECIAL_GENERIC_ALIAS = typing._SpecialGenericAlias  # type: ignore
-UNION_GENERIC_ALIAS = typing._UnionGenericAlias  # type: ignore
-ALIAS_TYPE = [SPECIAL_GENERIC_ALIAS, types.GenericAlias]
-UNION_TYPE = [UNION_GENERIC_ALIAS, types.UnionType]
